289df70da655c280eb8fb55ff412baa140c39cc3
pvtien96 <pvtien96@gmail.com>
2020-09-13 01:29:43 +0700

    Complete tracking on groundtruth with SORT or DeepSORT

---
+++ gt2ds.py
@@ -0,0 +1,151 @@
+import argparse
+import os
+import time
+from distutils.util import strtobool
+import cv2
+import json
+import numpy as np
+
+from sort import Sort
+from deep_sort import DeepSort
+
+from util import draw_bboxes, draw_polys
+
+def main():
+    args = get_parser().parse_args()
+    if args.display:
+        cv2.namedWindow("Groundtruth2(Deep)Sort", cv2.WINDOW_NORMAL)
+    sort = Sort()
+    deepsort = DeepSort(args.deepsort_checkpoint, nms_max_overlap=args.nms_max_overlap, use_cuda=bool(strtobool(args.use_cuda)))
+    assert os.path.isfile(os.path.join(args.input, 'via_export_json.json')), "Error: path error, via_export_json.json not found"
+    if args.out_vid:
+        out_vid = cv2.VideoWriter(
+            filename=args.out_vid,
+            fourcc=cv2.VideoWriter_fourcc(*'MJPG'),
+            fps=1.0,
+            frameSize=(1920, 1440),
+        )
+    if args.out_txt:
+        out_txt = open(args.out_txt, "w+")
+
+    total_counter = [0]*1000
+    json_file = os.path.join(args.input, 'via_export_json.json')
+    with open(json_file) as f:
+        imgs_anns = json.load(f)
+    for idx, v in enumerate(imgs_anns.values()):
+        filename = os.path.join(args.input, v["filename"])
+        annos = v["regions"]        
+        polys = []
+        dets = []
+        for anno in annos:
+            region_attributes = anno["region_attributes"]
+            if not region_attributes:
+                break
+            anno = anno["shape_attributes"]
+            if anno["name"] != "polygon":
+                break
+            px = anno["all_points_x"]
+            py = anno["all_points_y"]
+            poly = np.array([[x, y] for x, y in zip(px, py)], np.int32).reshape((-1,1,2))
+            if int(region_attributes["category_id"])==1:
+                dets.append([np.min(px), np.min(py), np.max(px), np.max(py), 1])
+                polys.append(poly)
+        start = time.time()
+        im = cv2.imread(filename)
+        current_counter = []
+        if args.tracker == 'sort':
+            if len(dets):
+                dets = np.array(dets)
+            else:
+                dets = np.empty((0,5))
+            outputs = sort.update(dets)
+            outputs = np.array([element.clip(min=0) for element in outputs]).astype(int)
+        else:
+            ccwh_boxes = []
+            for det in dets:
+                ccwh_boxes.append([(det[0]+det[2])/2, (det[1]+det[3])/2, det[2]-det[0], det[3]-det[1]])  
+            ccwh_boxes = np.array(ccwh_boxes)
+            confidences = np.ones(len(dets))
+            outputs, __ = deepsort.update(ccwh_boxes, confidences, im)
+        if len(outputs):
+            tlbr_boxes = outputs[:, :4]
+            identities = current_counter = outputs[:, -1]
+            ordered_identities = []
+            for identity in identities:
+                if not total_counter[identity]:
+                    total_counter[identity] = max(total_counter) + 1
+                ordered_identities.append(total_counter[identity])
+            im = draw_bboxes(im, tlbr_boxes, ordered_identities, binary_masks=[])
+            if args.out_txt:
+                for i in range(len(ordered_identities)):
+                    tlbr = tlbr_boxes[i]
+                    line = [idx+1, ordered_identities[i], tlbr[0], tlbr[1], tlbr[2]-tlbr[0], tlbr[3]-tlbr[1], 1, 1, 1]
+                    out_txt.write(",".join(str(item) for item in line) + "\n")
+        end = time.time()
+        im = draw_polys(im, polys)
+        im = cv2.putText(im, "Frame ID: "+str(idx), (20,20), 0, 5e-3 * 200, (0,255,0), 2) 
+        time_fps = "Time: {}s, fps: {}".format(round(end - start, 2), round(1 / (end - start), 2))            
+        im = cv2.putText(im, "Total Hand Counter: "+str(max(total_counter)), (20, 120), 0, 5e-3 * 200, (0,255,0), 2)
+        im = cv2.putText(im, "Current Hand Counter: "+str(len(current_counter)),(20, 80), 0, 5e-3 * 200, (0,255,0), 2)
+        im = cv2.putText(im, time_fps,(20, 40), 0, 5e-3 * 200, (0,255,0), 3)      
+        if args.display:
+            cv2.imshow("out_vid", im)
+            cv2.waitKey(0)
+        if args.out_vid:
+            out_vid.write(im)
+            
+def get_parser():
+    parser = argparse.ArgumentParser(description="Grounttruth to (Deep)SORT demo")
+    parser.add_argument("--input", 
+         type=str,
+         default='/media/data3/EgoCentric_Nafosted/non_skip/train/',
+         help='path to input folder contains detection groundtruth', 
+    )
+    parser.add_argument("--tracker",
+        type=str,
+        default='sort',
+        help='tracker type, sort or deepsort',
+    )
+    parser.add_argument("--deepsort_checkpoint",
+        type=str,
+        default="deep_sort/deep/checkpoint/ckpt.t7",
+        help='Cosine metric learning model checkpoint',
+    )
+    parser.add_argument(
+        "--max_dist",
+        type=float, 
+        default=0.3,
+        help="Max cosine distance",
+    )
+    parser.add_argument("--nms_max_overlap",
+        type=float,
+        default=0.5,
+        help='Non-max suppression threshold',
+    )
+    parser.add_argument(
+        "--ignore_display", 
+        dest="display", 
+        action="store_false"
+    )
+    parser.add_argument(
+        "--out_vid", 
+        type=str, 
+        default="output_video.avi",
+        help="Output video",
+    )
+    parser.add_argument(
+        "--use_cuda", 
+        type=str, 
+        default="True",
+        help="Use GPU if true, else use CPU only",
+    )
+    parser.add_argument(
+        "--out_txt",
+        type=str,
+        default="/media/data3/EgoCentric_Nafosted/mot/test/GH010383_5_462_968_1.txt",
+        help="Write tracking results in MOT16 format to file seqtxt2write. To evaluate using pymotmetrics",
+    )
+    return parser
+
+if __name__ == "__main__":
+    main()
---
+++ loop_gt2ds.py
@@ -0,0 +1,49 @@
+#recusively inference on groundtruth of detection
+import os
+from tqdm import tqdm
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="inference vids")
+    parser.add_argument(
+        "--root_path",
+        type=str,
+        default='/media/data3/EgoCentric_Nafosted/non_skip/train/',
+        help="Root path",
+    )
+    parser.add_argument(
+        "--tracker",
+        type=str,
+        default="sort",
+        help="sort or deepsort",
+    )
+    parser.add_argument(
+        "--out_vids",
+        type=str,
+        default='/media/data3/EgoCentric_Nafosted/non_skip/out_vids/',
+        help='Path to folder to save output videos',
+    )
+    parser.add_argument(
+        "--out_txts",
+        type=str,
+        default='/media/data3/EgoCentric_Nafosted/mot/test/',
+        help='Path to folder to save output sequence texts',
+    )
+    return parser
+
+def main():
+    args = get_parser().parse_args()
+    directories = os.listdir(args.root_path)
+    for i in tqdm(range(len(directories))):
+        directory = directories[i]
+        input_path = os.path.join(args.root_path, directory) 
+        out_vid = os.path.join(args.out_vids, directory+'.avi')
+        out_txt = os.path.join(args.out_txts, directory+'.txt')
+        command = 'python3 gt2ds.py --input ' + input_path + ' --tracker ' + args.tracker + ' --ignore_display ' + ' --out_vid ' + out_vid + ' --out_txt ' + out_txt
+        print(command)
+        os.system(command)
+
+    return 0
+
+if __name__ == "__main__":
+    main()
